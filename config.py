3c6201f23333a456cc5fe9a17a35835bbfb76f2b
SpotDylan <120763426+SpotDylan@users.noreply.github.com>
2022-12-16 21:04:25 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+#This file simply provides our keys, both the API and Secret Key that will be fed to the trade class so that it knows where to send data
+APCA_API_KEY_ID = "PKXT8C13JI4L8JJ7WC88"
+APCA_API_SECRET_KEY = "NnfKVarDvAkUZE28kjlXTTWBFncJ4bW8VIR80tDq"
+APCA_API_DATA_URL = "https://data.alpaca.markets"
+
